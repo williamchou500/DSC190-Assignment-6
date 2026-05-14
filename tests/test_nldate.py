0a8018ad61330642cef7a357c560a84ca68fd785
williamchou500 <williamchou500@gmail.com>
2026-05-13 18:43:30 -0700

    added test and nldate.py

---
+++ tests/test_nldate.py
@@ -0,0 +1,38 @@
+from dsc190_assignment_6 import nldate
+from datetime import date, timedelta
+
+def test_today():
+    assert nldate.parse("Today") == date.today()
+
+def test_tomorrow():
+    assert nldate.parse("Tomorrow") == date.today() + timedelta(days=1)
+
+def test_days_before():
+    assert nldate.parse("2 days before tomorrow", date(2026,5,13)) == date(2026,5,12)
+
+def test_days_after():
+    assert nldate.parse("5 days after today", date(2026, 5, 13)) == date(2026, 5, 18)
+
+def test_weeks_before():
+    assert nldate.parse("2 weeks before today", date(2026, 5, 13)) == date(2026, 4, 29)
+
+def test_weeks_after():
+    assert nldate.parse("1 week after tomorrow", date(2026, 5, 13)) == date(2026, 5, 21)
+
+def test_months_before():
+    assert nldate.parse("1 month before today", date(2026, 5, 13)) == date(2026, 4, 13)
+
+def test_months_after():
+    assert nldate.parse("1 month after tomorrow", date(2026, 5, 13)) == date(2026, 6, 14)
+
+def test_years_before():
+    assert nldate.parse("3 years before today", date(2026, 5, 13)) == date(2023, 5, 13)
+
+def test_years_after():
+    assert nldate.parse("10 years before tomorrow", date(2026, 5, 13)) == date(2016, 5, 14)
+
+def test_all_before():
+    assert nldate.parse("2 years, 1 week, 3 months, and 5 days before today", date(2026, 5, 13)) == date(2024, 2, 1)
+
+def test_all_after():
+    assert nldate.parse("2 years, 1 week, 3 months, and 5 days after tomorrow", date(2026, 5, 13)) == date(2028, 8, 26)
